bbc536faf5c76cc13ff31883fb660218852202e9
yinhaoxuan <1723010279@qq.com>
2019-06-09 10:31:49 +0800

    Add files via upload

---
+++ chip.h
@@ -0,0 +1,26 @@
+
+class Visualizer;
+
+
+typedef vector<Point> Path;
+class Chip
+{
+private:
+    int m,n;
+    vector<vector<int> > map;//>=0:concentration -1:none
+    Path route[2],BRoute,SRoute,discardRoute;
+    bool getB,getS,discard;
+    /* data */
+public:
+    friend class Visualizer;
+    Chip(/* args */);
+    ~Chip();
+};
+
+Chip::Chip(/* args */)
+{
+}
+
+Chip::~Chip()
+{
+}
